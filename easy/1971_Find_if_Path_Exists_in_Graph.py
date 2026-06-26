fcd26607b1d467bdb69fcecd98e1d4ed732c6a8e
HamzaLaaz <laazhamza502@gmail.com>
2026-06-26 23:45:29 +0100

    finish easy/1971_Find_if_Path_Exists_in_Graph.py

---
+++ easy/1971_Find_if_Path_Exists_in_Graph.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+
+def validPath(self, n: int, edges: list[list[int]], source: int, destination: int) -> bool:
+    graph = {}
+    for i in range(n):
+        graph[i] = []
+    for x, y in edges:
+        graph[x].append(y)
+        graph[y].append(x)
+    queue = deque()
+    queue.append(source)
+    visited = {source}
+    while queue:
+        vertex = queue.popleft()
+        if vertex == destination:
+            return True
+        for d in graph[vertex]:
+            if d not in visited:
+                visited.add(d)
+                queue.append(d)
+    return False
