80b2d86e51598abe63281f9b1456cf789bab2d24
HamzaLaaz <laazhamza502@gmail.com>
2026-07-03 12:53:12 +0100

    finish easy/1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.py

---
+++ easy/1342_Number_of_Steps_to_Reduce_a_Number_to_Zero.py
@@ -0,0 +1,10 @@
+def numberOfSteps(num: int) -> int:
+    count = 0
+    while num > 0:
+        if num % 2 != 0:
+            num -= 1
+            count += 1
+        else:
+            num //= 2
+            count += 1
+    return count
